2838bcf7adc72ecc05422f4d1f8460694c30173a
tomas <anabosouza1218@gmail.com>
2026-05-21 11:00:52 -0500

    fix: basename do Router para GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -684,7 +684,7 @@ const Index = () => {
 
 export default function App() {
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<Index />} />
         <Route path="/admin" element={<AdminRoute />} />
